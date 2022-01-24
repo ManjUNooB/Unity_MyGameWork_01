90d9afd1ccf5b922697d76618ed46f9a0de50983
ManjUNooB <ryuzin20000901@gmail.com>
2022-01-25 01:34:15 +0900

    ドア開閉用のスクリプト追加　RoomのFront部分のドア部分をくり抜くために複数のオブジェクトで構成  Roomの構成を変更

---
+++ MyGameWork_01/Assets/Scripts/GameObject/DoorScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    //  ドアのエリアに入っているか
+    private bool isArea;
+
+    //  ドアのアニメーター
+    private Animator animator;
+
+    //  ドアの開閉キー
+    public KeyCode openKey = KeyCode.E;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isArea = false;
+        animator = transform.parent.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(openKey) && isArea)
+		{
+            animator.SetBool("Open", !animator.GetBool("Open"));
+		}
+    }
+    
+    void OnTriggerEnter(Collider col)
+	{
+        if(col.tag == "Player") isArea = true;
+		
+	}
+
+    void OnTriggerExit(Collider col)
+	{
+        if(col.tag == "Player") isArea = false;
+		
+	}
+
+}
