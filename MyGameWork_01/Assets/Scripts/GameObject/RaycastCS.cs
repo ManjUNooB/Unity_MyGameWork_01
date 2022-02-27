b606c498a014a53aa8a005ddc3b32d57d5224d6e
ManjUNooB <ryuzin20000901@gmail.com>
2022-02-28 00:31:33 +0900

    一応レイキャストはできた？デバッグ表示はできた

--- MyGameWork_01/Assets/Scripts/GameObject/RaycastCS.cs
+++ MyGameWork_01/Assets/Scripts/GameObject/RaycastCS.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class RaycastCS : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] private Camera playerCamera;
+	[SerializeField]
+	[Range(0, 2)] private int MouseKey;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+
+	private void FixedUpdate()
+	{
+
+		if(Input.GetMouseButton(MouseKey)){
+			Ray ray = playerCamera.ViewportPointToRay(new Vector2(0.5f, 0.5f));
+			RaycastHit hit;
+
+			if(Physics.Raycast(ray,out hit,10.0f)){
+				Debug.Log(hit.collider.gameObject.name);
+			}
+			Debug.DrawRay(ray.origin, ray.direction * 10, Color.red, 5);
+		}
+
+
+	}
 }
